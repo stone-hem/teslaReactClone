e334eb4e51d088ad28a890cb6aca5d21385b1b67
First-Last <first-last@kali.kali>
2022-03-24 08:48:53 +0300

    cool sections

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,7 +5,54 @@ import Section from './Section';
 function Homepage() {
   return (
       <Container>
-          <Section/>
+          <Section
+          title="Model S"
+          description="Order online for touchless delivery"
+          backgroundImg="model-s.jpg"
+          LeftBtnText="Custom Order"
+          RightBtnText="Existing Inventory"
+          />
+          <Section
+          title="Model Y"
+          description="Order online for touchless delivery"
+          backgroundImg="model-y.jpg"
+          LeftBtnText="Custom Order"
+          RightBtnText="Existing Inventory"
+          />
+          <Section
+          title="Model X"
+          description="Order online for touchless delivery"
+          backgroundImg="model-x.jpg"
+          LeftBtnText="Custom Order"
+          RightBtnText="Existing Inventory"
+          />
+          <Section
+          title="Model 3"
+          description="Order online for touchless delivery"
+          backgroundImg="model-3.jpg"
+          LeftBtnText="Custom Order"
+          RightBtnText="Existing Inventory"
+          />
+           <Section
+          title="Lowest Cost Solar Panels in America"
+          description="Money-back guarantee"
+          backgroundImg="solar-panel.jpg"
+          LeftBtnText="Order now"
+          RightBtnText="Learn more"
+          />
+           <Section
+          title="Solar for new roofs"
+          description="Solar roofs costs less than a new roof"
+          backgroundImg="solar-roof.jpg"
+          LeftBtnText="Order now"
+          RightBtnText="Learn more"
+          />
+           <Section
+          title="Accessories"
+          description=""
+          backgroundImg="accessories.jpg"
+          LeftBtnText="Order now"
+          />
       </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
-  return (
-    <Wrap>
-        <TopText>
-            <h1>Model s</h1>
-            <p>Order online for touchless delevery</p>
-        </TopText>
-        <Buttons>
-        <BottonButtons>
-            <Left>
-                Custom Order
-            </Left>
-            <Right>
-                Existing Inventory
-            </Right>
-        </BottonButtons>
-        <Down src="/images/down-arrow.svg" />
-        </Buttons>
-    </Wrap>
-  )
+function Section({ title, description, LeftBtnText, RightBtnText, backgroundImg }) {
+    return (
+        <Wrap bgImage={backgroundImg} >
+            <TopText>
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </TopText>
+            <Buttons>
+                <BottonButtons>
+                    <Left>
+                        {LeftBtnText}
+                    </Left>
+                    {RightBtnText &&
+                        <Right>
+                            {RightBtnText}
+                        </Right>
+                    }
+                </BottonButtons>
+                <Down src="/images/down-arrow.svg" />
+            </Buttons>
+        </Wrap>
+    )
 }
 
 export default Section
 
-const Wrap=styled.div`
+const Wrap = styled.div`
 width:100vw; //100
 height:100vh; //100
 background-size:cover; //cover the exact whole page
@@ -33,26 +35,30 @@ background-position:center; //position it centrally
 background-repeat:no-repeat; //no bg repeat
 background-image:url('/images/model-s.jpg'); //access the image in public folder
 
-//align the butons to the bottom
+//align the buttons to the bottom
 display:flex;
 flex-direction:column; //from top to bottom
-justify-content:space-between; //vertical allignment
-align-items:center; //horizontal allignment
+justify-content:space-between; //vertical alignment
+align-items:center; //horizontal alignment
+background-image:${props => `url("/images/${props.bgImage}")`};
+
 `;
 
-const TopText=styled.div`
+const TopText = styled.div`
 padding-top:15vh;
 text-align:center;
 `;
 
-const BottonButtons=styled.div`
+const BottonButtons = styled.div`
 display:flex; //display the buttons in a flex manner next to each other
 margin-bottom:30px; //move up 30px
-cursor:pointer;
+@media(max-width:760px){
+    flex-direction:column; //at >760 display column
+}
 
 `;
 
-const Left=styled.div`
+const Left = styled.div`
 background-color: rgba(23,26,32,0.8); //bg color
 height:40px; //horizontally
 width:256px; //vertically
@@ -64,18 +70,23 @@ border-radius:300px; //round the border of the boxes
 opacity:0.8; //transparency
 font-size:14px;  //size of the font
 margin:10px;
+cursor:pointer;
 `;
 
-const Right=styled(Left)`
-
+const Right = styled(Left)`
+background:white;
+opacity:0.6;
+color:black;
 
 `
 
-const Down=styled.img`
+const Down = styled.img`
 margin-top:20px;
 height:40px;
+overflow-x:hidden;
+animation: animateDown infinite 1.5s;  //defined in the index css as @keyframes
 `
 
-const Buttons=styled.div`
-animate: animateDown infinite 1.5s;
+const Buttons = styled.div`
+
 `
